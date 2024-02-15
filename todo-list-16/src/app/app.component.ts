9ac9f7c82c75a312a08aa1574ebc08410abaeb44
franciele Franco <franciele.alves1108@gmail.com>
2024-02-15 15:42:29 -0300

    Implementing Everything Card - Part 2

--- todo-list-16/src/app/app.component.ts
+++ todo-list-16/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, HeaderComponent, TodoCardComponent],
 })
 export class AppComponent {
   title = 'todo-list-16';
